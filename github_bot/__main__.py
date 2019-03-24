d3cd9b2df64055508c474b857266607b1d521bfd
Jan Ferko <julyloov@gmail.com>
2019-03-24 14:02:01 +0100

    Initial commit

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == '__main__':
+    run_app(
+        name='Comment-Reactor-PyconSK-by-iref',
+        version='1.0.0',
+        url='https://github.com/apps/comment-reactor-pyconsk-iref'
+    )
